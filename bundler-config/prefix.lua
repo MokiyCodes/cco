94f6fdb3f9ce91265dbefc1cd85de79ff4f0f503
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-11-14 01:31:58 +0100

    feat: :sparkles: initial commit

---
+++ bundler-config/prefix.lua
@@ -0,0 +1,26 @@
+-- Yielding's Bundler Prefix Script
+-- Forked & Stripped by Mokiy
+-- Copyright (c) 2022 YieldingExploiter.
+-- Copyright (c) 2022 MokiyCodes.
+-- MIT License
+
+local null = nil -- null is better than nil, change my mind
+local modules = {} -- we will assign modules to here later
+local require = function(...) -- handle loading modules
+  local requested, returned = { ... }, {}
+  for _, filepath in pairs(requested) do
+    if not modules[filepath] then
+      error('[blb] no such module \'' .. filepath .. '\'')
+    end
+    local module = modules[filepath]
+    if module.isCached then
+      table.insert(returned, module.cache)
+    else
+      local moduleValue = module.load()
+      module.cache = moduleValue
+      module.isCached = true
+      table.insert(returned, module.cache)
+    end
+  end
+  return table.unpack(returned)
+end
