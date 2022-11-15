d95a005ec027a66437dc0658863f1a59bf97e4f0
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-11-15 11:46:58 +0100

    feat: :sparkles: allow loading from external things

--- src/boot.lua
+++ src/boot.lua
@@ -15,14 +15,33 @@ return function()
   _G._g = _G
   console.clear()
   console.centerLog 'Loading Frontend...'
-  if not fs.exists '.cco/setting-dm.txt' then
-    local f = fs.open('.cco/setting-dm.txt', 'w')
+  if not fs.exists '/.cco/setting-dm.txt' then
+    local f = fs.open('/.cco/setting-dm.txt', 'w')
     f.write 'basalt-or-lite'
     f.close()
   end
+  if not fs.exists '/.cco/frontends/' then
+    fs.makeDir '/.cco/frontends'
+  end
   local frontend
-  local frontendFile = fs.open '.cco/setting-dm.txt'
+  local frontendFile = fs.open('/.cco/setting-dm.txt', 'r')
   frontend = frontendFile.readAll()
   frontendFile.close()
-  require('frontends/' .. frontend)
+  if fs.exists('/.cco/frontends/' .. frontend .. '.lua') then
+    local frontendScript = fs.open('/.cco/frontends/' .. frontend .. '.lua', 'r')
+    local frontendCode = frontendScript.readAll()
+    frontendScript.close()
+    ---@diagnostic disable-next-line: deprecated
+    local byte, err = (loadstring or load)(frontendCode)
+    if type(byte) ~= 'function' then
+      error('Compilation Error: ' .. err)
+    end
+    local rt = byte()
+    if type(rt) == 'function' then
+      rt = rt()
+    end
+    return rt
+  else
+    return require('frontends/' .. frontend)()
+  end
 end
